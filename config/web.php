fdbf478177e59a4ab203434203e126b1e0692ffc
andku83 <andreykushnarev83@gmail.com>
2018-09-23 06:32:54 +0300

    add ip to debugger

--- config/web.php
+++ config/web.php
@@ -79,7 +79,7 @@ if (YII_DEBUG && YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1', '93.76.187.134'],
     ];
 
     $config['bootstrap'][] = 'gii';
